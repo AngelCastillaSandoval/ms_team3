df74becaf9458a5236ea62bf68f99358d2591f1a
AngelCastillaSandoval <angel.castilla@vallegrande.edu.pe>
2025-05-01 11:54:55 -0500

    WORKSHOP

--- src/main/java/pe/edu/vallegrande/workshop/WorkshopApplication.java
+++ src/main/java/pe/edu/vallegrande/workshop/WorkshopApplication.java
@@ -8,10 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // Carga el archivo .env automáticamente
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
 		SpringApplication.run(WorkshopApplication.class, args);
 	}
 }
