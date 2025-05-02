b1918b7ee3371f28ba68154926b52d995a3529f7
AngelCastillaSandoval <angel.castilla@vallegrande.edu.pe>
2025-05-02 07:10:55 -0500

    :v

--- src/main/java/pe/edu/vallegrande/issue/service/KafkaProducerService.java
+++ src/main/java/pe/edu/vallegrande/issue/service/KafkaProducerService.java
@@ -1,15 +1,17 @@
 package pe.edu.vallegrande.issue.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import pe.edu.vallegrande.issue.dto.IssueKafkaEventDto;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
@@ -17,16 +19,13 @@ public class KafkaProducerService {
 
     private static final String TOPIC = "issue-events";
 
-    /**
-     * 🔹 Envía un evento relacionado a una incidencia al topic "issue-events".
-     */
-    public void sendIssueEvent(IssueKafkaEventDto eventDto) {
+    public void sendWorkshopEvent(IssueKafkaEventDto eventDto) {
         try {
             String message = objectMapper.writeValueAsString(eventDto);
             kafkaTemplate.send(TOPIC, String.valueOf(eventDto.getId()), message);
-            log.info("📤 Evento enviado a Kafka -> Topic: {}, Key: {}, Payload: {}", TOPIC, eventDto.getId(), message);
+            log.info("✅ Evento enviado a Kafka -> Topic: {}, Key: {}, Payload: {}", TOPIC, eventDto.getId(), message);
         } catch (Exception e) {
-            log.error("❌ Error al enviar evento a Kafka: {}", e.getMessage(), e);
+            log.error("❌ Error al enviar evento a Kafka", e);
         }
     }
 }
